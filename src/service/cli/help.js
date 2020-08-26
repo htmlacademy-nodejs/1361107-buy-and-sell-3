cb4d4966b798a3ad41405aa32b940231c46b9557
Roman Belov <rnbelov96@gmail.com>
2020-08-26 08:02:41 +0300

    Добавленая табуляция для команды --help

--- src/service/cli/help.js
+++ src/service/cli/help.js
@@ -6,13 +6,13 @@ module.exports = {
     console.log(`
     Программа запускает http-сервер и формирует файл с данными для API.
 
-    Гайд:
-    service.js <command>
+      Гайд:
+      service.js <command>
     
-    Команды:
-    --version:            выводит номер версии
-    --help:               печатает этот текст
-    --generate <count>    формирует файл mocks.json
+      Команды:
+      --version:            выводит номер версии
+      --help:               печатает этот текст
+      --generate <count>    формирует файл mocks.json
     `);
   },
 };
