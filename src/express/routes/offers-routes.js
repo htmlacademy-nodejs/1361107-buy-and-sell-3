b067b01efde2a6d21389198d6cc4a600fbb7c503
Belov Roman <rnbelov96@gmail.com>
2020-11-04 12:54:10 +0300

    Настраивает редактирование объявления

--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -1,12 +1,20 @@
 "use strict";
 
 const {Router} = require(`express`);
+const api = require(`../api`).getAPI();
 
 const offersRouter = new Router();
 
 offersRouter.get(`/category/:id`, (req, res) => res.render(`category`));
 offersRouter.get(`/add`, (req, res) => res.render(`new-ticket`));
-offersRouter.get(`/edit/:id`, (req, res) => res.render(`ticket-edit`));
+offersRouter.get(`/edit/:id`, async (req, res) => {
+  const {id} = req.params;
+  const [offer, categories] = await Promise.all([
+    api.getOffer(id),
+    api.getCategories()
+  ]);
+  res.render(`ticket-edit`, {offer, categories});
+});
 offersRouter.get(`/:id`, (req, res) => res.render(`ticket`));
 
 module.exports = offersRouter;
