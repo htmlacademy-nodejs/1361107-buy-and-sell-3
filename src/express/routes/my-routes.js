32218646b4fe571c3575fa8aad13567cebda7ebb
Belov Roman <rnbelov96@gmail.com>
2020-11-04 12:37:20 +0300

    Добавляет отображение списка комментариев

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -9,6 +9,9 @@ myRouter.get(`/`, async (req, res) => {
   const listOffers = await api.getOffers();
   res.render(`my-tickets`, {listOffers});
 });
-myRouter.get(`/comments`, (req, res) => res.render(`comments`));
+myRouter.get(`/comments`, async (req, res) => {
+  const listOffers = await api.getOffers();
+  res.render(`comments`, {listOffers: listOffers.slice(0, 3)});
+});
 
 module.exports = myRouter;
