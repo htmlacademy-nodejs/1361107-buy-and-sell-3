f1e2f1db94cd1ddbe5b79a889f568678c99284db
Belov Roman <rnbelov96@gmail.com>
2020-11-04 12:24:42 +0300

    Добавляет отображение статей на личной странице

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -1,10 +1,14 @@
 "use strict";
 
 const {Router} = require(`express`);
+const api = require(`../api`).getAPI();
 
 const myRouter = new Router();
 
-myRouter.get(`/`, (req, res) => res.render(`my-tickets`));
+myRouter.get(`/`, async (req, res) => {
+  const listOffers = await api.getOffers();
+  res.render(`my-tickets`, {listOffers});
+});
 myRouter.get(`/comments`, (req, res) => res.render(`comments`));
 
 module.exports = myRouter;
