72a0575a43bab1e94c3aa8087f920bad2fcf9f78
Belov Roman <rnbelov96@gmail.com>
2021-01-09 15:57:55 +0300

    Исправляет проверку авторизации

--- src/express/middleware/check-authorization.js
+++ src/express/middleware/check-authorization.js
@@ -1,18 +1,20 @@
 "use strict";
 
 const {HttpCode, ResponseMessage} = require(`../../constants`);
+const {catchAsync} = require(`../../utils`);
+const api = require(`../api`).getAPI();
 
-module.exports = (req, res, next) => {
+module.exports = catchAsync(async (req, res, next) => {
   const {user} = req.session;
   const {id: offerId} = req.params;
-  if (!user.offers.includes(Number(offerId))) {
-    res
-      .status(HttpCode.BAD_REQUEST)
-      .render(`errors/400`, {
-        statusCode: HttpCode.FORBIDDEN,
-        message: ResponseMessage.FORBIDDEN,
-      });
+  const offer = await api.getOffer(offerId);
+
+  if (!offer.owner.email !== user.email) {
+    res.status(HttpCode.BAD_REQUEST).render(`errors/400`, {
+      statusCode: HttpCode.FORBIDDEN,
+      message: ResponseMessage.FORBIDDEN,
+    });
   }
 
   return next();
-};
+});
